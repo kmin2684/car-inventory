716ba51b270870cb1f9c17180a992d5f67d9424f
kmin2684 <65061813+kmin2684@users.noreply.github.com>
2022-05-30 22:39:13 -0600

    money formatted and edit medu removed

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,6 +1,7 @@
 import { DataGrid, GridApi, GridCellValue, GridColumns } from '@mui/x-data-grid';
 import {cars} from '../../data/data';
 import { Button } from '@mui/material';
+import { numberWithCommas } from '../../utils/utilFunctions';
 
 
 export const columns: GridColumns = [
@@ -8,12 +9,15 @@ export const columns: GridColumns = [
     { field: 'make', headerName: 'Make', width: 130 },
     { field: 'model', headerName: 'Model', width: 130 },
     { field: 'year', headerName: 'Year', width: 130 },
-    { field: 'price', headerName: 'Price', width: 130, valueGetter: (params: {row: {price: number}}) => `${params.row.price}` },
+    { field: 'price', headerName: 'Price', width: 130, valueGetter: (params: {row: {price: number}}) => `$${numberWithCommas(params.row.price)}` },
     { field: 'isLive', headerName: 'Status', width: 130, valueGetter: (params: {row: {isLive: Boolean}}) => params.row.isLive ? "Live" : "Sold"},
     {
         field: 'edit',
         headerName: '',
+        width: 130, 
         sortable: false,
+        hideSortIcons: true,
+        disableColumnMenu: true,
         renderCell: (params : any) => {
           const onClick = (e: any) => {
             e.stopPropagation(); // don't select this row after clicking
@@ -39,7 +43,7 @@ export const columns: GridColumns = [
 const DataTable: React.FC = () => {
 
     return (
-        <div style={{ height: 400, width: '720px' }}>
+        <div style={{ height: 400, width: '860px' }}>
           <DataGrid
             rows={cars}
             columns={columns}
