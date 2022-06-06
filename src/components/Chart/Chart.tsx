cbe9c98ed18e53179d9175c28eafbe0a219d0c2c
kmin2684 <65061813+kmin2684@users.noreply.github.com>
2022-06-06 06:06:05 -0600

    responsive drawer deleted

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -11,21 +11,6 @@ import {
   Legend
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-
-];
 
 function getMainWidth() {
   return document.querySelector('body')!.clientWidth;
